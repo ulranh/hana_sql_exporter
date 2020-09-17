67db7c2554db9ee40f558f78610aaf1f3cf827fb
ulli <ulrich.anhalt@halbgera.de>
2020-09-17 07:55:50 +0200

    comment pprof rules

--- cmd/exporter.go
+++ cmd/exporter.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
-	"net/http/pprof"
 	"strconv"
 	"strings"
 	"sync"
@@ -102,22 +101,22 @@ func (config *Config) web(flags map[string]*string) error {
 	mux.HandleFunc("/", rootHandler)
 
 	// Add the pprof routes
-	mux.HandleFunc("/debug/pprof/", pprof.Index)
-	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	// mux.HandleFunc("/debug/pprof/", pprof.Index)
+	// mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	// mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	// mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	// mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 
-	mux.Handle("/debug/pprof/block", pprof.Handler("block"))
-	mux.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
-	mux.Handle("/debug/pprof/heap", pprof.Handler("heap"))
-	mux.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
+	// mux.Handle("/debug/pprof/block", pprof.Handler("block"))
+	// mux.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+	// mux.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+	// mux.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
 
 	server := &http.Server{
-		Addr:    ":" + *flags["port"],
-		Handler: mux,
-		// WriteTimeout: 10 * time.Second,
-		// ReadTimeout:  10 * time.Second,
+		Addr:         ":" + *flags["port"],
+		Handler:      mux,
+		WriteTimeout: 10 * time.Second,
+		ReadTimeout:  10 * time.Second,
 	}
 	err = server.ListenAndServe()
 	if err != nil {
